7de5267a732cd658abfdac2c61917fd6265aa3bc
Dastan Akhmetzhanov <58333215+dakhmetz@users.noreply.github.com>
2019-12-27 21:29:44 +0500

    fix: fixed SMTP mail sender

---
+++ src/send.php
@@ -0,0 +1,40 @@
+<?php
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone'];
+$userQuestion = $_POST['userQuestion'];
+
+// Load Composer's autoloader
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'dastan.rich@gmail.com';                     // SMTP username
+    $mail->Password   = 'gmaildastan327845';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('dastan.rich@gmail.com', 'Dastan');
+    $mail->addAddress('dastan.rich@yandex.ru', 'Client');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Username: ${userName}, Phone number: ${userPhone}, Email: ${userEmail}. Question - ${userQuestion}";
+
+    $mail->send();
+    header('Location: thanks.html')
+} catch (Exception $e) {
+    echo "Письмо не отправлено. Код ошибки: {$mail->ErrorInfo}";
+}
